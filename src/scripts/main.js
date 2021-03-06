67a451c4d86170778287cab13367a42bc45b5231
JacksonRGoodman <jacksonrgoodman@gmail.com>
2021-03-05 21:37:08 -0600

    ERD conformity

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,3 +22,36 @@ showPostList();
 };
 // Are you defining the function here or invoking it?
 startGiffyGram();
+
+const songs = [
+	{
+        title:"MAAD WORLD",
+	    artist:"toastydigital,{Kanye West & Kendrick Lamar}",
+        audioFile:"05 MAAD WORLD.mp3",
+        albumArt:"/playlist/GKTF cover.png",
+        year:"2021",
+		timeLength:144
+    },
+    {
+        title:"Can't Tell Me Nothing'",
+	    artist:"toastydigital,{Kanye West 2049}",
+        audioFile:"08 CAN'T TELL ME NOTHIN.mp3",
+        albumArt:"/playlist/Kanye 2049.png",
+        year:"2020",
+		timeLength:126
+    },
+];
+
+const playlistAsHTML = (songObj) => {
+	return `<div>
+		<p>__${songObj.title} --${songObj.artist}, ${songObj.year}</p>
+		<audio class="playlistPlayer" src="/src/audio/${songObj.audioFile}" controls preload="auto" />
+    </div>`
+};
+
+const songListContainer = document.querySelector(".musicBox")
+let allSongsAsHTML = ""
+for (const song of songs) {
+    allSongsAsHTML += playlistAsHTML(song)
+}
+songListContainer.innerHTML += allSongsAsHTML;
