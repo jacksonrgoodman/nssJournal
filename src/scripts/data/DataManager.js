89365a3ce334c599023fdbead873082be4103b91
JacksonRGoodman <jacksonrgoodman@gmail.com>
2021-03-10 15:29:08 -0600

    reduced the javascript and media player shelf, now its hidden

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -2,29 +2,3 @@ export const getPosts = () => {
     return fetch("http://localhost:8088/entries")
     .then(response => response.json())
 };
-
-
-
-
-//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-
-
-export const getPlaylistImages = () => {
-    return fetch("http://localhost:8088/randomPlaylistArt")
-    .then(response => response.json())
-};
-
-
-
-
-
-
-
-
-
-
-
-export const getSoundcloud = () => {
-    return fetch("http://localhost:8088/playlist")
-    .then(response => response.json())
-};
--- src/scripts/soundcloud/soundlcoud.js
+++
@@ -1,5 +0,0 @@
-export const soundcloudPost = (postObject) => {
-    return `
-      <iframe width="1465" height="320" scrolling="no" frameborder="no" allow="autoplay" src="${postObject.soundcloud}"></iframe>
-    `
-  }
